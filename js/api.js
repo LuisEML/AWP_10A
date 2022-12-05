d0f73f1768ea00f5b179d89171b775e47c105b48
LuiEML <guero560mlb@gmail.com>
2022-12-05 03:07:48 -0600

    modificaciones nuevas

--- js/api.js
+++ js/api.js
@@ -1,71 +1,51 @@
+const input = document.querySelector('#searchInput')
+const userList = document.querySelector('#users')
 
- let contenido = document.querySelector(".contenido");
- function traerDatos() {
-    fetch("json.json")
-    .then(respuesta => respuesta.json())
-    .then(datos=>{
-        tabla(datos)
-    })
+let users = []
+window.addEventListener('DOMContentLoaded', async ()=>{
+ const data = await loadUsers()
+ let busqueda = document.getElementById("busqueda");
+     busqueda.addEventListener("change", () => {
+       let buscar = busqueda.value;
+       users = data[buscar]
+       console.log(users)
+       renderUsers(users)    
+     });
+})
+
+async function loadUsers() {
+    const response = await fetch("json.json")
+   return  await response.json()
  }
 
- function tabla(datos) {
-    contenido.innerHTML = '';
-    for(let i of datos.gatos) {
-        console.log(i)
-        contenido.innerHTML += `
-        <div class="col">
-        <div style="border-color:#E1E1EF;" class="card">
-          <img src='${i.img}' class="card-img-top" width="450" height="450" alt="...">
-          <div class="card-body">
-            <p class="card-text text-end">${i.id}</p>
-            <h5 class="card-title"><b>Nombre: </b>${i.nombre}</h5>
-            <p class="card-text"><b>Descripción: </b>${i.descripcion}</p>
-          </div>
-          <ul class="list-group list-group-flush">
-              <li class="list-group-item"><b>Personalidad: </b>${i.personalidad}</li>
-              <li class="list-group-item"><b>Salud: </b>${i.salud}</li>
-              <li class="list-group-item"><b>País de Origen: </b>${i.origen}</li>
-            </ul>
-        </div>
-      </div>
-     `;
-    }
+input.addEventListener('keyup',e =>{
+    const newUsers = users.filter(user => user.nombre.toLowerCase().includes(input.value.toLowerCase()))
+    renderUsers(newUsers);
+    console.log(input.value)
+ })
+
+ const createUserItems = users => users.map(user => `
+ <div class="col">
+ <div style="border-color:#E1E1EF;" class="card">
+   <img src='${user.img}' class="mover card-img-top" width="450px" height="450px">
+   <div class="card-body">
+     <p class="card-text text-end">${user.id}</p>
+     <h5 class="card-title"><b>Nombre: </b>${user.nombre}</h5>
+     <p class="card-text"><b>Descripción: </b>${user.descripcion}</p>
+   </div>
+   <ul class="list-group list-group-flush">
+       <li class="list-group-item"><b>Personalidad: </b>${user.personalidad}</li>
+       <li class="list-group-item"><b>Salud: </b>${user.salud}</li>
+       <li class="list-group-item"><b>País de Origen: </b>${user.origen}</li>
+     </ul>
+ </div>
+</div>
+ `).join(' ');
+
+ function renderUsers(users){
+    const itemsString = createUserItems(users)
+    userList.innerHTML = itemsString
  }
-
- let contenido2 = document.querySelector(".contenido2");
-
- function traerDogs() {
-  fetch("json.json")
-  .then(res => res.json())
-  .then(dogs=>{
-      tabla1(dogs)
-  })
-}
-
-function tabla1(dogs) {
-  contenido2.innerHTML = '';
-  for(let i of dogs.perros) {
-      console.log(i)
-      contenido2.innerHTML += `
-      <div class="col">
-      <div style="border-color:#E1E1EF;" class="card">
-        <img src='${i.img}' class="card-img-top" width="450" height="450" alt="...">
-        <div class="card-body">
-          <p class="card-text text-end">${i.id}</p>
-          <h5 class="card-title"><b>Nombre: </b>${i.nombre}</h5>
-          <p class="card-text"><b>Descripción: </b>${i.descripcion}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item"><b>Personalidad: </b>${i.personalidad}</li>
-            <li class="list-group-item"><b>Salud: </b>${i.salud}</li>
-            <li class="list-group-item"><b>País de Origen: </b>${i.origen}</li>
-          </ul>
-      </div>
-    </div>
-   `;
-  }
-}
-
 
  
 
